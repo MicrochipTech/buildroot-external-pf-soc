98c5db935bccd29d41f05a657f706712265786d0
Matt Wood <matt.wood@microchip.com>
2021-03-26 16:15:20 -0400

    Generate ext4 based image
    Changed u-boot to use an env file in the boot partition which is
    generated by host-uboot-utils from u-boot.env.txt
    Also removed uImage generation since it is not used and renamed script
    to a more fitting name.
    
    Signed-off-by: Matt Wood <matt.wood@microchip.com>

--- board/microchip/icicle/fitimage.sh
+++ board/microchip/icicle/fitimage.sh
@@ -3,7 +3,6 @@
 ITS_DIR=$BR2_EXTERNAL_ICICLE_PATH/board/microchip/icicle
 
 $HOST_DIR/bin/riscv64-linux-objcopy -O binary $BINARIES_DIR/vmlinux $BINARIES_DIR/vmlinux.bin
-$HOST_DIR/bin/mkimage -A riscv -O linux -T kernel -C "none" -a 80200000 -e 80200000 -d $BINARIES_DIR/vmlinux.bin $BINARIES_DIR/uImage
 
 cd $BINARIES_DIR
 cp $ITS_DIR/osbi-fit-image.its $BINARIES_DIR
