2242946707058ba01c2ce3e7c301df5f91532a24
Matt Wood <matt.wood@microchip.com>
2021-03-29 14:04:14 -0400

    Move devicetree build from script to kernel
    Remove the post-build script for building the devicetree binary as it
    can be handled by Buildroot and the kernel itself.
    
    Update the icicle kit defconfig for building the dtb out-of-tree.
    
    Signed-off-by: Matt Wood <matt.wood@microchip.com>

--- board/microchip/icicle/dts/build_dts.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-DTS_DIR=$BR2_EXTERNAL_ICICLE_PATH/board/microchip/icicle/dts
-cp $DTS_DIR/../icicle-kit-es.dts $DTS_DIR
-$HOST_DIR/bin/riscv64-linux-gcc -E -Wp,-MD,$DTS_DIR/icicle-kit-es.dts -nostdinc -I. -D__ASSEMBLY__ -undef -D__DTS__ -x assembler-with-cpp -o $DTS_DIR/icicle-kit-es.dtb.dts.tmp $DTS_DIR/icicle-kit-es.dts
-$HOST_DIR/bin/dtc -O dtb -o $BINARIES_DIR/icicle-kit-es.dtb -b 0 -i ./ -R 4 -p 0x1000 -d $DTS_DIR/icicle-kit-es.dtb.d.dtc.tmp $DTS_DIR/icicle-kit-es.dtb.dts.tmp
